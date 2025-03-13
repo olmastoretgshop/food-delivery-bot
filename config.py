a0b1d5a52b7d0a68be096edd5f68465c96a1dd57
olmastoretgshop <123176458+olmastoretgshop@users.noreply.github.com>
2025-03-13 13:11:39 +0500

    Update config.py

--- config.py
+++ config.py
@@ -1,3 +1,3 @@
 # Configuration file
-BOT_TOKEN = "5944259328:AAFcHoyZ0kDmHRp4AktlGNkirIebPWE7n0U"  # Replace with your bot token
-ADMIN_ID = 276373613          # Replace with your admin Telegram ID
+BOT_TOKEN = "5944259328:abcdfghijklmnopqrst"  # Replace with your bot token
+ADMIN_ID = 123456789          # Replace with your admin Telegram ID
